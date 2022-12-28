3a24bccd78ddca4384be108f756ea7a645cb8d29
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2022-12-28 13:40:01 -0400

    Ejercicios del dia 12: 28-12-2022
    Hecho el ejercicio 1

---
+++ Ejercicio.js
@@ -0,0 +1,15 @@
+///todo: Ejercicio del dia 12: 12/28/2022
+
+//?1-Usa el método "test"
+
+/*
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Cambia esta línea //Respuesta
+
+console.log(result);
+*/
+
+//?2-Haz coincidir cadenas literales
+
+
